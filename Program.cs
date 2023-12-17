33fd536dce0fba1da2bf53897391f365ab7881f2
Gulnara <gula10119@yandex.ru>
2023-12-17 12:51:15 +0300

    4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+﻿string[] arr1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[arr1.Length];
+void SecondArrayWithIF(string[] arr1, string[] array2)
 {
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < arr1.Length; i++)
     {
     if(array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
+        array2[count] = arr1[i];
         count++;
         }
     }
@@ -20,5 +20,5 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
-SecondArrayWithIF(array1, array2);
+SecondArrayWithIF(arr1, array2);
 PrintArray(array2);
